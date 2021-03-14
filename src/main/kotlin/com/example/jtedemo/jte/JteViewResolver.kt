86c6e8d37e96759394b34f5fb22b524f2f0a0be0
Zach Tibbitts <zachtib@gmail.com>
2021-03-14 14:22:25 -0400

    Use correct classloader, and flush output stream after writing

--- src/main/kotlin/com/example/jtedemo/jte/JteViewResolver.kt
+++ src/main/kotlin/com/example/jtedemo/jte/JteViewResolver.kt
@@ -6,11 +6,17 @@ import gg.jte.resolve.DirectoryCodeResolver
 import org.springframework.web.servlet.View
 import org.springframework.web.servlet.ViewResolver
 import java.nio.file.Path
+import java.nio.file.Paths
 import java.util.*
 
 class JteViewResolver(templatePath: Path) : ViewResolver {
     private val codeResolver: DirectoryCodeResolver = DirectoryCodeResolver(templatePath)
-    private val templateEngine: TemplateEngine = TemplateEngine.create(codeResolver, ContentType.Html)
+    private val templateEngine: TemplateEngine = TemplateEngine.create(
+        codeResolver,
+        Paths.get("jte-classes"),
+        ContentType.Html,
+        javaClass.classLoader
+    )
 
     override fun resolveViewName(viewName: String, locale: Locale): View? {
         val filename: String? = codeResolver.resolve(viewName)
--- src/main/kotlin/com/example/jtedemo/jte/OutputStreamTemplateOutput.kt
+++ src/main/kotlin/com/example/jtedemo/jte/OutputStreamTemplateOutput.kt
@@ -4,12 +4,18 @@ import gg.jte.TemplateOutput
 import java.io.OutputStream
 import java.io.Writer
 
-class OutputStreamTemplateOutput(private val outputStream: OutputStream) : TemplateOutput {
-    override fun getWriter(): Writer = outputStream.writer()
+class OutputStreamTemplateOutput(outputStream: OutputStream) : TemplateOutput {
+
+    private val streamWriter = outputStream.bufferedWriter()
+
+    override fun getWriter(): Writer = streamWriter
 
     override fun writeContent(value: String?) {
         if (value != null) {
-            writer.write(value)
+            with(streamWriter) {
+                write(value)
+                flush()
+            }
         }
     }
 }
